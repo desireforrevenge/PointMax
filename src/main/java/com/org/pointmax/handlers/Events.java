db50f25be094c8268162baefb332fda79a03d92b
who i am <raftalya@inbox.ru>
2024-07-30 00:52:01 +0500

    event listener

---
+++ src/main/java/com/org/pointmax/handlers/Events.java
@@ -0,0 +1,12 @@
+package com.org.pointmax.handlers;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+
+public class Events implements Listener {
+    @EventHandler
+    public void onMessage(PlayerCommandPreprocessEvent preprocessEvent) {
+
+    }
+}
